ed2aebfcba8dd2293b15456fa3f894c5bf8550f8
meghana151 <126710660+meghana151@users.noreply.github.com>
2025-07-09 21:23:24 +0900

    Delete preprocessing directory

--- preprocessing/normalise.py
+++
@@ -1,4 +0,0 @@
-def scale_values(values):
-    scaler = StandardScaler()
-    scaled = scaler.fit_transform(values.reshape(-1, 1)).flatten()
-    return scaled, scaler
--- preprocessing/windowing.py
+++
@@ -1,2 +0,0 @@
-def create_windows(data, w_size):
-    return np.array([data[i:i+w_size] for i in range(len(data)-w_size+1)])
